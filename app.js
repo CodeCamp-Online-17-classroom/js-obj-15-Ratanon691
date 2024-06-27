0955194c8d566089e3a2272015cfda1f76dec01a
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-27 12:56:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// write code here
+const notebook = {
+  brand: 'ASUS',
+  model: 'Vivobook D413IA-EB303TS',
+  processor: 'AMD Ryzen 7 4700U 3.6GHz',
+  graphics: 'Integrated Graphics : AMD Radeon Graphics',
+  ram: '8GB DDR4 Onboard',
+  storage: '512GB PCIe NVMe M.2 SSD'
+};
+let obj1 = {}
+for(key in notebook){
+  obj1[key] = notebook[key]
+}
+console.log(obj1)
+
+let obj2 = Object.assign({}, notebook)
+console.log(obj2)
